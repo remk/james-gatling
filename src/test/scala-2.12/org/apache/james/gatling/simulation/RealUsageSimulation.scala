ea87dd0cebbc376bceddd8e71f3014c8b960f2f0
Rémi Kowalski <rkowalski@linagora.com>
2019-04-17 17:42:59 +0200

    fixup! add RealUsageSimulation simulation

--- src/test/scala-2.12/org/apache/james/gatling/simulation/RealUsageSimulation.scala
+++ src/test/scala-2.12/org/apache/james/gatling/simulation/RealUsageSimulation.scala
@@ -6,7 +6,6 @@ import io.gatling.core.scenario.Simulation
 import io.gatling.core.structure.ScenarioBuilder
 import org.apache.james.gatling.control.{Password, User, UserFeeder, Username}
 import org.apache.james.gatling.jmap.scenari.realusage.{InboxHomeLoading, JMapUtils, JmapInboxHomeLoadingScenario, RealUsageScenario}
-import org.apache.james.gatling.simulation.jmap.DefaultPortMapping
 import org.apache.james.gatling.simulation.utils.UsageFrequency
 import org.slf4j
 import org.slf4j.LoggerFactory
@@ -14,7 +13,7 @@ import org.slf4j.LoggerFactory
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class RealUsageSimulation extends Simulation with DefaultPortMapping {
+class RealUsageSimulation extends Simulation  {
   private val logger: slf4j.Logger = LoggerFactory.getLogger(this.getClass.getCanonicalName)
 
   private val NB_USERS = 100
@@ -23,7 +22,7 @@ class RealUsageSimulation extends Simulation with DefaultPortMapping {
 
   private val JMAP_PORT = 80
 
-  private val jmapUrl = s"http://localhost:${getMappedPort(JMAP_PORT)}"
+  private val jmapUrl = s"http://localhost:$JMAP_PORT"
   private val getUsers: Seq[User] = (0 until NB_USERS).map(i => User(Username(s"user$i@open-paas.org"), Password("secret")))
 
   private val feeder: SourceFeederBuilder[String] = UserFeeder.toFeeder(getUsers).circular
@@ -33,7 +32,7 @@ class RealUsageSimulation extends Simulation with DefaultPortMapping {
       pace(usageFrequency.paceFromFrequency).exec(scenarioBuilder)
     )
     .inject(rampUsers(NB_USERS) during (1 minute))
-    .protocols(HttpSettings.httpProtocol(getMappedPort: Int => Int))
+    .protocols(HttpSettings.httpProtocol)
 
 
   private def buildMaxScenarioResponseTimeAssertion(scenario: RealUsageScenario, maxResponseTime: Duration) = {
