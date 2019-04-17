fecba0552a6588ea35d978784c5f49da8654a2dc
Rémi Kowalski <rkowalski@linagora.com>
2019-04-17 17:19:25 +0200

    refactor JamesServer in it

--- src/it/scala-2.12/org/apache/james/gatling/JmapInboxHomeLoadingIT.scala
+++ src/it/scala-2.12/org/apache/james/gatling/JmapInboxHomeLoadingIT.scala
@@ -5,28 +5,12 @@ import org.apache.james.gatling.jmap.scenari.realusage.{JMapUtils, JmapInboxHome
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
-class JmapInboxHomeLoadingIT extends JmapIT {
-
-  private def execCommand(command: String) = {
-    val rt = Runtime.getRuntime
-    val copyPr = rt.exec(command)
-    copyPr.waitFor()
-  }
-
-  before {
-    val emlPath = getClass.getResource("/message.eml").getPath
-    val shPath = getClass.getResource("/create_mails_and_mailboxes.sh").getPath
-
-    execCommand(s"docker cp  ${emlPath} ${server.containerId}:/message.eml")
-    execCommand(s"docker cp ${shPath} ${server.containerId}:/create_mails_and_mailboxes.sh")
-    execCommand(s"docker exec ${server.containerId}  /create_mails_and_mailboxes.sh")
-    Thread.sleep(1000)
-  }
+class JmapInboxHomeLoadingIT extends JmapIT(importMessages = true) {
 
   scenario(feederBuilder => {
     val oneMainMailbox = 1
     val oneSubMailbox = 1
-    lazy val mainMailboxesIdsByIndexForUsers = Await.result(JMapUtils.mainMailboxesIdsByIndexForUsers(s"http://localhost:${server.mappedJmapPort}", users), 2 seconds)
+    lazy val mainMailboxesIdsByIndexForUsers = Await.result(JMapUtils.mainMailboxesIdsByIndexForUsers(s"http://localhost:${mappedJmapPort}", users), 2 seconds)
 
     new JmapInboxHomeLoadingScenario().generate(feederBuilder, mainMailboxesIdsByIndexForUsers, oneMainMailbox, oneSubMailbox)
   })
